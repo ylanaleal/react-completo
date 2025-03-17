d1d22520f35221f63c875d2a0f27f69f4db64025
Ylana Oliveira <ylanalmo@gmail.com>
2025-03-17 13:29:25 -0300

    textarea

--- 10.useRef/src/App.jsx
+++ 10.useRef/src/App.jsx
@@ -21,25 +21,41 @@ const App = () => {
 
   return (
     <>
-    <h1>Lista de comentários</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          ref={inputElement}
-        />
-        <br />
-        <button onClick={handleClick}>Enviar</button>
-        <button onClick={handleDelete} style={{ marginLeft: '10px' }}>
-          Limpar
-        </button>
-        <ul>
-          {comentarios.map((comentario, index) => (
-            <li key={index}>{comentario}</li>
-          ))}
-        </ul>
-      </form>
+      <div style={{ width: '600px' }}>
+        <h1>Lista de comentários</h1>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            ref={inputElement}
+          />
+          <br />
+          <div style={{ marginBottom: '30px' }}>
+            <button onClick={handleClick}>Enviar</button>
+            <button onClick={handleDelete} style={{ marginLeft: '10px' }}>
+              Limpar
+            </button>
+          </div>
+          <h3>Comentários</h3>
+          <ul style={{ padding: '0' }}>
+            {comentarios.map((comentario, index) => (
+              <li
+                key={index}
+                style={{
+                  padding: '20px',
+                  marginTop: '15px',
+                  boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
+                  listStyle: 'none',
+                  wordWrap: 'break-word',
+                  overflow: 'hidden',
+                }}
+              >
+                {comentario}
+              </li>
+            ))}
+          </ul>
+        </form>
+      </div>
     </>
   );
 };
